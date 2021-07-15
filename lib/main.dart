93d8b8ef0ea04fea934fb24a7b18d02a8ec1bded
Fernando Binasco <fernando.binasco@gmail.com>
2021-07-15 15:46:13 -0300

    construindo a interface

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,24 @@ const key = "6b796750";
 const request = "https://api.hgbrasil.com/finance?key=" + key;
 
 void main() async {
-  print(await getData());
-
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
 Future<Map> getData() async {
   http.Response response = await http.get(Uri.parse(request));
+  print(json.decode(response.body)["results"]["currencies"]["USD"]["buy"]);
   return json.decode(response.body);
 }
 
@@ -31,6 +40,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar = 0.0;
+  double euro = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,27 +59,63 @@ class _HomeState extends State<Home> {
             case ConnectionState.none:
             case ConnectionState.waiting:
               return Center(
-                child: Text("Carregando dados...",
-                  style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 25.0
-                  ),
+                child: Text(
+                  "Carregando dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
                   textAlign: TextAlign.center,
                 ),
               );
             default:
-              if(snapshot.hasError){
+              if (snapshot.hasError) {
                 return Center(
-                  child: Text("Erro ao carregar dados =(",
-                    style: TextStyle(
-                        color: Colors.amber,
-                        fontSize: 25.0
-                    ),
+                  child: Text(
+                    "Erro ao carregar dados =(",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
                     textAlign: TextAlign.center,
                   ),
                 );
               } else {
-                return Container(color: Colors.green,);
+                dolar = snapshot.data?["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data?["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150.0,
+                        color: Colors.amber,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "R\$"),
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Dólares",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "U\$"),
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Euros",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "€"),
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      )
+                    ],
+                  ),
+                );
               }
           }
         },
@@ -75,5 +123,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-
