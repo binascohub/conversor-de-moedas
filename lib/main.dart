8d1a9c834b8a79646f12e2fdb167de0b4a7825a9
Fernando Binasco <fernando.binasco@gmail.com>
2021-07-15 14:53:36 -0300

    trabalhando com o futuro

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
+
 // command: flutter pub add http
 import 'package:http/http.dart' as http;
 
 // generate your key on https://console.hgbrasil.com/keys/new_key_plan
 const key = "6b796750";
-const request = "https://api.hgbrasil.com/finance?key="+key;
+const request = "https://api.hgbrasil.com/finance?key=" + key;
 
 void main() async {
-
   print(await getData());
 
   runApp(MaterialApp(
@@ -39,7 +40,38 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
-
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text("Carregando dados...",
+                  style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25.0
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if(snapshot.hasError){
+                return Center(
+                  child: Text("Erro ao carregar dados =(",
+                    style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 25.0
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(color: Colors.green,);
+              }
+          }
+        },
+      ),
     );
   }
 }
